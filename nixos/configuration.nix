e30af439d4d9033ef955949362120fd00d53c084
hnsylzg <hnsylzg@gmail.com>
2025-09-17 18:38:14 +0800

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -129,6 +129,7 @@
       # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
       # Be sure to change it (using passwd) after rebooting!
       initialPassword = "771029";
+      description = "lizhengang";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
