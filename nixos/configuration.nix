73d0c779653dba20e613d6fcf5123f6d0efe0bb3
hnsylzg <hnsylzg@gmail.com>
2025-11-26 12:50:43 +0800

    commit

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -70,7 +70,7 @@
     flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
   in {
     settings = {
-      trusted-users = [ "lzg" ];
+      extra-trusted-users = [ "lzg" ];
       # Enable flakes and new 'nix' command
       experimental-features = "nix-command flakes";
       # Opinionated: disable global registry
